1ec671651be497b3dfd04c018c87f5cbbef6fc39
Ivan Davydov <dawydoffiv@yandex.ru>
2024-03-12 16:15:15 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from application.db.people import get_employees
+from application.salary import calculate_salary
+from datetime import datetime
+
+if __name__ == '__main__':
+    print(datetime.now())
+    get_employees()
+    calculate_salary()
+
+
+import art
+
+print(art.text2art("Habr", font='block'))
